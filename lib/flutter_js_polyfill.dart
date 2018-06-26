dae42d4c7a71f37d7f7aedf170cb29b29f9e42b4
John Ryan <john.p.ryan4@gmail.com>
2018-06-25 17:02:33 -0700

    remove JSObject for Dartium + chrome compatibility

--- lib/flutter_js_polyfill.dart
+++ lib/flutter_js_polyfill.dart
@@ -5,17 +5,3 @@ import "dart:js" as dartjs;
 Function/*=F*/ allowInterop/*<F extends Function>*/(Function/*=F*/ f) {
   return dartjs.allowInterop(f);
 }
-
-class JsObject extends dartjs.JsObject {
-  factory JsObject(dartjs.JsFunction constructor, [List arguments]) {
-    return new dartjs.JsObject(constructor, arguments);
-  }
-
-  factory JsObject.fromBrowserObject(object) {
-    return new dartjs.JsObject.fromBrowserObject(object);
-  }
-
-  factory JsObject.jsify(object) {
-    return new dartjs.JsObject.jsify(object);
-  }
-}
