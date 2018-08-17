035a2e713a4aadbd928cfa89a98952a16e6b616a
John Ryan <john.p.ryan4@gmail.com>
2018-08-17 16:20:56 -0700

    dart 2 update

--- lib/flutter_js_polyfill.dart
+++ lib/flutter_js_polyfill.dart
@@ -2,6 +2,6 @@ library flutter_js_polyfill;
 
 import "dart:js" as dartjs;
 
-Function/*=F*/ allowInterop/*<F extends Function>*/(Function/*=F*/ f) {
+F allowInterop<F extends Function>(F f) {
   return dartjs.allowInterop(f);
 }
